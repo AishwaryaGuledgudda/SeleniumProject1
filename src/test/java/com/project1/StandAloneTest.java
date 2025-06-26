104aba22b8bd03925dc431c4b54297c6aed0a03e
Aishwarya Guledgudda <aishwaryaguledgudda@gmail.com>
2025-06-26 12:15:33 +0100

    Selenium Project 1

--- src/test/java/com/project1/StandAloneTest.java
+++ src/test/java/com/project1/StandAloneTest.java
@@ -6,10 +6,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
+import javax.swing.*;
 import java.time.Duration;
 import java.util.List;
 
@@ -30,9 +33,28 @@ public class StandAloneTest {
         // stream helps to iterate through the list of products
         assert zaraProduct != null;
         zaraProduct.findElement(By.cssSelector(".card-body button:last-of-type")).click();
-       Thread.sleep(5000);
+        Thread.sleep(5000);
         //wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector("[routerlink*='cart']"))));
         driver.findElement(By.cssSelector("[routerlink*='cart']")).click();
        // driver.quit();
+        List<WebElement> cartProducts = driver.findElements(By.cssSelector(".cartSection h3"));
+        Boolean match = cartProducts.stream().anyMatch(cartProduct->cartProduct.getText().equalsIgnoreCase("ZARA COAT 3"));
+        //anyMatch looks for the element if matches it returns Boolean value
+        Assert.assertTrue(match);
+        //if match has "true" value then test passses if "false" then the TC fails
+        driver.findElement(By.cssSelector(".totalRow button")).click();
+
+        //Using Action Class to send keys to dropdown
+        Actions a = new Actions(driver);
+        a.sendKeys(driver.findElement(By.cssSelector("input[placeholder='Select Country']")),"india").build().perform();
+        //wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("ta-results")));
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("(//button[contains(@class,'ta-item')])[2]")).click();
+        driver.findElement(By.cssSelector(".action__submit")).click();
+        String confirmMsg = driver.findElement(By.cssSelector(".hero-primary")).getText();
+        Assert.assertTrue(confirmMsg.equalsIgnoreCase("THANKYOU FOR THE ORDER."));
+        driver.close();
+
+
     }
 }
